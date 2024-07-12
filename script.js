aca1c3fceb71e9c7ca28a441d24202cbc0d0c086
Maggie24-tech <107142910+Maggie24-tech@users.noreply.github.com>
2024-07-12 13:14:56 -0600

    improved the UX

--- script.js
+++ script.js
@@ -12,9 +12,14 @@ function generatePoem(event) {
 
   let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "84t11b56aaaafb0o5f17756cdb05fc3d";
-  let prompt = `User instructions: Generate a French poem about ${instructionsInput.value}`;
+  let prompt = `User instructions: Generate a English poem about ${instructionsInput.value}`;
   let context =
     "You are a romantic Poem expert and love to write short poems. You mission is to generate a 4 line poem in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem and NOT at the beginning";
+
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class="generating">⏳ Generating a poem about ${instructionsInput.value}</div>`;
+
   console.log("Generating poem");
   console.log(`Prompt: ${prompt}`);
   console.log(`Context: ${context}`);
